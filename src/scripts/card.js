868450085a625ed76e75766c75b01e15e8429832
Glenn Raya <rayaglenn@gmail.com>
2023-11-09 11:27:57 +0800

    Add tilting animations to elements

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,35 +1,37 @@
 // All element with a class of 'card' will be skewed animated upon hover.
-const card = document.querySelector('.card')
+const cards = document.querySelectorAll('[data-tilter]')
 
-gsap.set(card, { transformPerspective: 1000 })
+cards.forEach(card => {
+    gsap.set(card, { transformPerspective: 1000 })
 
-card.addEventListener('mouseenter', () => {
-    gsap.to(card, {
-        duration: 0.2,
-        scale: 1.05,
-        ease: 'power2.out'
+    card.addEventListener('mouseenter', () => {
+        gsap.to(card, {
+            duration: 0.2,
+            scale: 1.05,
+            ease: 'power2.out'
+        })
     })
-})
 
-card.addEventListener('mousemove', e => {
-    const rect = card.getBoundingClientRect()
-    const x = ((e.clientX - rect.left) / card.clientWidth - 0.5) * 2
-    const y = ((e.clientY - rect.top) / card.clientHeight - 0.5) * 2
+    card.addEventListener('mousemove', e => {
+        const rect = card.getBoundingClientRect()
+        const x = ((e.clientX - rect.left) / card.clientWidth - 0.5) * 2
+        const y = ((e.clientY - rect.top) / card.clientHeight - 0.5) * 2
 
-    gsap.to(card, {
-        duration: 0.5,
-        rotationY: x * 15,
-        rotationX: y * -15,
-        ease: 'power2.out'
+        gsap.to(card, {
+            duration: 0.5,
+            rotationY: x * 15,
+            rotationX: y * -15,
+            ease: 'power2.out'
+        })
     })
-})
 
-card.addEventListener('mouseleave', () => {
-    gsap.to(card, {
-        duration: 0.5,
-        rotationY: 0,
-        rotationX: 0,
-        scale: 1,
-        ease: 'power2.out'
+    card.addEventListener('mouseleave', () => {
+        gsap.to(card, {
+            duration: 0.5,
+            rotationY: 0,
+            rotationX: 0,
+            scale: 1,
+            ease: 'power2.out'
+        })
     })
 })
