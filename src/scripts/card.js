b029cfc11ccc90e0107027b86c6fd001c2855eba
Glenn Raya <rayaglenn@gmail.com>
2023-11-10 11:21:38 +0800

    Add who we are section and new images

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,37 +1,37 @@
 // All element with an data-tilter html attribute will be skewed animated upon hover.
-const elements = document.querySelectorAll('[data-tilter]')
+const elements = document.querySelectorAll("[data-tilter]");
 
-elements.forEach(element => {
-    gsap.set(element, { transformPerspective: 1000 })
+elements.forEach((element) => {
+    gsap.set(element, { transformPerspective: 1000 });
 
-    element.addEventListener('mouseenter', () => {
+    element.addEventListener("mouseenter", () => {
         gsap.to(element, {
             duration: 0.2,
             scale: 1.05,
-            ease: 'power2.out'
-        })
-    })
+            ease: "power2.out",
+        });
+    });
 
-    element.addEventListener('mousemove', e => {
-        const rect = element.getBoundingClientRect()
-        const x = ((e.clientX - rect.left) / element.clientWidth - 0.5) * 2
-        const y = ((e.clientY - rect.top) / element.clientHeight - 0.5) * 2
+    element.addEventListener("mousemove", (e) => {
+        const rect = element.getBoundingClientRect();
+        const x = ((e.clientX - rect.left) / element.clientWidth - 0.5) * 2;
+        const y = ((e.clientY - rect.top) / element.clientHeight - 0.5) * 2;
 
         gsap.to(element, {
             duration: 0.5,
             rotationY: x * 15,
             rotationX: y * -15,
-            ease: 'power2.out'
-        })
-    })
+            ease: "power2.out",
+        });
+    });
 
-    element.addEventListener('mouseleave', () => {
+    element.addEventListener("mouseleave", () => {
         gsap.to(element, {
             duration: 0.5,
             rotationY: 0,
             rotationX: 0,
             scale: 1,
-            ease: 'power2.out'
-        })
-    })
-})
+            ease: "power2.out",
+        });
+    });
+});
