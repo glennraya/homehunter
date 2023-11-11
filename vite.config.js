3b02ecee268907f1c171eec41485deee410b418c
Glenn Raya <rayaglenn@gmail.com>
2023-11-12 00:05:28 +0800

    Add Terser and reconfigure vite

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,14 @@
-export default {
-    base: "",
-    // other configurations...
-};
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    build: {
+        minify: "terser", // Default is 'terser', but you can also use 'esbuild'
+        terserOptions: {
+            compress: {
+                drop_console: true, // Remove console logs for production
+                // Other terser options here
+            },
+        },
+    },
+    // Other configurations...
+});
