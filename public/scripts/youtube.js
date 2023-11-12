ab87536658f0f5232cc3be35da423f14a81d71ff
Glenn Raya <rayaglenn@gmail.com>
2023-11-12 10:47:21 +0800

    Modify YouTube dialog show animation

--- public/scripts/youtube.js
+++ public/scripts/youtube.js
@@ -1,13 +1,18 @@
+// Show the Youtube dialog
 document
     .getElementById("videoTrigger")
     .addEventListener("click", function (event) {
         event.preventDefault();
         var videoDialog = document.getElementById("videoDialog");
-        videoDialog.classList.add("visible");
         document.getElementById("videoDialog").style.display = "block";
+
+        requestAnimationFrame(function () {
+            videoDialog.classList.add("visible");
+        });
         document.getElementById("videoDialog").style.justifyContent = "center";
     });
 
+// Hide the Youtube dialog
 document.getElementById("closeButton").addEventListener("click", function () {
     var videoIframe = document.getElementById("youtubeVideo");
     var videoSrc = videoIframe.src;
@@ -16,8 +21,15 @@ document.getElementById("closeButton").addEventListener("click", function () {
 
     var videoDialog = document.getElementById("videoDialog");
     videoDialog.classList.remove("visible");
-    // To ensure the dialog is not display:block when fully transparent
-    setTimeout(function () {
-        videoDialog.style.display = "none";
-    }, 200); // This duration should match the CSS transition duration
+
+    videoDialog.addEventListener(
+        "transitionend",
+        function () {
+            // Only set to display none after the fade out transition ends
+            if (!videoDialog.classList.contains("visible")) {
+                videoDialog.style.display = "none";
+            }
+        },
+        { once: true },
+    ); // Ensures the event listener is removed after it fires once
 });
